e7ae8a338e4b4337a13fe2864ebd6fef3e40f371
norun9 <u.t012807@gmail.com>
2024-07-14 04:22:54 +0900

    scroll to last smoothly

--- client/src/components/layout/Messages.tsx
+++ client/src/components/layout/Messages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import { useFetch, useWebSocket } from '@/hooks'
 import { IMessageRes } from '@/types/api/response'
@@ -6,7 +6,15 @@ import MessageInput from '@/components/ui/forms/MessageInput'
 
 const Messages: React.FC = React.memo(() => {
   const { data: fetchedMessages } = useFetch<IMessageRes[]>('/v1/messages')
+  const lastEventRef = useRef<HTMLDivElement>(null)
   const webSocketURL = process.env.NEXT_PUBLIC_WEB_SOCKET_URL
+
+  const scrollToLastEvent = () => {
+    if (lastEventRef.current) {
+      lastEventRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
   const { socketRef, messages, setMessages } = useWebSocket(webSocketURL)
 
   const groupMessagesByDate = (messages: IMessageRes[] | undefined): Record<string, IMessageRes[]> => {
@@ -43,6 +51,10 @@ const Messages: React.FC = React.memo(() => {
 
   const groupedMessages: Record<string, IMessageRes[]> = groupMessagesByDate(messages)
 
+  useEffect(() => {
+    scrollToLastEvent()
+  }, [groupedMessages])
+
   return (
     <div className='flex-1 flex flex-col bg-gray-50 overflow-hidden'>
       <div className='border-b border-gray-border-3 flex px-6 py-2 items-center flex-none shadow-xl'>
@@ -55,25 +67,31 @@ const Messages: React.FC = React.memo(() => {
       </div>
       {/* Chat messages */}
       <div className='flex-1 overflow-y-auto px-6 py-4'>
-        {Object.entries(groupedMessages).map(([date, msgs]) => (
+        {Object.entries(groupedMessages).map(([date, msgs], dateIndex, dateArray) => (
           <div key={date}>
             <div className='flex items-center w-full'>
               <div className='flex-grow border-t border-gray-border-3'></div>
               <span className='mx-2 text-xs font-bold text-gray-400'>{date}</span>
               <div className='flex-grow border-t border-gray-border-3'></div>
             </div>
-            {msgs.map((msg: IMessageRes, index: number) => (
-              <div key={index} className='pt-3 pb-7 flex items-start text-sm hover:bg-gray-750'>
-                <Image src='/assets/icon/user/free.svg' height={40} width={40} alt='free_icon' className='mr-3' />
-                <div className='flex-1 overflow-hidden'>
-                  <div className='flex items-center'>
-                    <span className='font-bold text-orange-400 cursor-pointer'>User</span>
-                    <span className='font-bold text-gray-400 text-xs ml-1'>{msg.createdAt}</span>
+            {msgs.map((msg: IMessageRes, index: number) => {
+              const isLastEvent: boolean = dateIndex === dateArray.length - 1 && index === msgs.length - 1
+              return (
+                <div
+                  ref={isLastEvent ? lastEventRef : null}
+                  key={index}
+                  className='pt-3 pb-7 flex items-start text-sm hover:bg-gray-750'>
+                  <Image src='/assets/icon/user/free.svg' height={40} width={40} alt='free_icon' className='mr-3' />
+                  <div className='flex-1 overflow-hidden'>
+                    <div className='flex items-center'>
+                      <span className='font-bold text-orange-400 cursor-pointer'>User</span>
+                      <span className='font-bold text-gray-400 text-xs ml-1'>{msg.createdAt}</span>
+                    </div>
+                    <p className='text-gray-light leading-normal'>{msg.content}</p>
                   </div>
-                  <p className='text-gray-light leading-normal'>{msg.content}</p>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
         ))}
       </div>
